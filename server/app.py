3828b4ef08748cd43263715170cc0ffe85d1ce88
Brandi L Mason <masonbrandi66@gmail.com>
2023-09-13 13:00:03 -0600

    backend completed

--- server/app.py
+++ server/app.py
@@ -3,15 +3,16 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request, render_template, Flask, session
+from flask import request, render_template, Flask, session, make_response
 from flask_restful import Resource
+import datetime
 
 # Local imports
 from config import app, db, api
 # Add your model imports
 from models import Yoga_Class, Yoga_SignUp, User, Community_Event_SignUp, Community_Event
 
-
+# would like to go back and be more thoughtful about the error messages
 
 
 @app.route('/')
@@ -20,6 +21,8 @@ def index():
 
 
 #logging in
+
+#login/logout tested and all routes are working -- did not test check session, certain it is working.
 class Login(Resource):
     def post(self):
         login = request.get_json()
@@ -33,92 +36,296 @@ class Login(Resource):
         return {'error': 'Unauthorized'}, 401
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        ###not sure why it's user_id -- might not be right but worked for phase4 project
+        if user:
+            return user.to_dict()
+        else:
+            return {'message':'401: Not Authorized'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {'message':'204: No Content'}
 
 
 
-#add query for post request for make sure username doesn't already exist
-# class Users(Resource):
-#     def get(self):
-#         pass
+#   ***USERS***
+#add query for post request for make sure username doesn't already exist - this needs to go on my models under my validation
+# need to figure out the role & membership stuff in the future, this is just a placeholder to get the post to work
 
-#     def post(self):
-#         pass
+#users tested and all routes are working
+class Users(Resource):
+    def get(self):
+        users = [u.to_dict() for u in User.query.all()]
+        return users, 200
 
-# class UsersById(Resource):
-#     def get(self):
-#         pass
+    def post(self):
+        fields = request.get_json()
+        new_user = User(
+            name=fields['name'],
+            email=fields['email'],
+            address=fields['address'],
+            role=fields.get('role','admin'),
+            membership_status=fields.get('membership_status', True),
+            password_hash=fields['password']
+            )
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
+        return new_user.to_dict(), 200
 
-#     def patch(self):
-#         pass
+#usersbyid tested and all routes are working
+class UsersById(Resource):
+    def get(self, id):
+        user_by_id = User.query.filter_by(id=id).first()
+        if user_by_id == None:
+            return {'errors': ['user not found']}, 404
+        else:
+            return user_by_id.to_dict(), 200
 
-#     def delete(self):
-#         pass
+    def patch(self, id):
+        user_by_id = User.query.filter_by(id=id).first()
+        if user_by_id == None:
+            return {'errors': ['user not found']}, 404
+        else:
+            fields = request.get_json()
+            try:
+                for attr in fields:
+                    setattr(user_by_id, attr, fields[attr])
+                    db.session.add(user_by_id)
+                    db.session.commit()
+                return user_by_id.to_dict(), 200  
+            except ValueError:
+                return {"errors": ["validation errors"]}, 422
 
-# class Yoga_Classes(Resource):
-#     def get(self):
-#         pass
+    def delete(self, id):
+        user_by_id = User.query.filter_by(id=id).first()
+        if user_by_id == None:
+            return {'errors': ['user not found']}, 404
+        else:
+            db.session.delete(user_by_id)
+            db.session.commit()
+            return {}, 200
 
-#     def post(self):
-#         pass
 
-# class YogaClassesById(Resource):
-#     def get(self):
-#         pass
+#yoga classes routes tested and working
+class Yoga_Classes(Resource):
+    def get(self):
+        yoga_classes = [y.to_dict() for y in Yoga_Class.query.all()]
+        return yoga_classes, 200
+    
+    #I want to post a new class by picking from the 3 class options we have - how can I add these to a drop down on the front end and post it on the backend? Like picking a class and it autofills the boxes of creating a new class.
 
-#     def patch(self):
-#         pass
+    def post(self):
+        #conversion for datetime - tested this in test.py
+        def convert(date_time):
+            format = '%b %d %Y %I:%M%p'
+            datetime_str = datetime.datetime.strptime(date_time, format)
+            return datetime_str
+        
+        fields = request.get_json()
+        new_class = Yoga_Class(
+                class_name=fields['class_name'],
+                class_description=fields['class_description'],
+                start_time =convert(fields['start_time']),
+                time_duration =fields['time_duration'],
+                teacher_id =fields['teacher_id']
+            )
+        db.session.add(new_class)
+        db.session.commit()
+        return new_class.to_dict(), 200
+        #this does return null and I think that's because I am not have it return anything?
 
-#     def delete(self):
-#         pass
 
-# class Community_Events(Resource):
-#     def get(self):
-#         pass
+#yogaclassesbyId routes tested and working
+class YogaClassesById(Resource):
+    def get(self, id):
+        class_by_id = Yoga_Class.query.filter_by(id=id).first()
+        if class_by_id == None:
+            return {'error':['class not found']}, 400
+        else:
+            return class_by_id, 200
 
-#     def post(self):
-#         pass
+    def patch(self, id):
+        class_by_id = Yoga_Class.query.filter_by(id=id).first()
+        if class_by_id == None:
+            return {'error': ['class not found']}, 404
+        else:
+            fields = request.get_json()
+            try:
+                for attr in fields:
+                    setattr(class_by_id, attr, fields[attr])
+                    db.session.add(class_by_id)
+                    db.session.commit()
+                return class_by_id.to_dict(), 200
+            except ValueError:
+                return {'error':['validation error']}, 422
 
-# class CommunityEventsById(Resource):
-#     def get(self):
-#         pass
+    def delete(self, id):
+        class_by_id = Yoga_Class.query.filter_by(id=id).first()
+        if class_by_id == None:
+            return {'error':['yoga class not found']}, 404
+        else:
+            db.session.delete(class_by_id)
+            db.session.commit()
+            return {}, 200
 
-#     def patch(self):
-#         pass
+# community events tested and routes working
+class Community_Events(Resource):
+    def get(self):
+        community_events = [c.to_dict() for c in Community_Event.query.all()]
+        return community_events, 200
 
-#     def delete(self):
-#         pass
+    def post(self):
+        #conversion for datetime - tested this in test.py
+        def convert(date_time):
+            format = '%b %d %Y %I:%M%p'
+            datetime_str = datetime.datetime.strptime(date_time, format)
+            return datetime_str
+        fields = request.get_json()
+        new_event = Community_Event(
+            event_name=fields['event_name'],
+            event_description=fields['event_description'],
+            start_time=convert(fields['start_time']),
+            time_duration=fields['time_duration'],
+            location=fields['location']
+        )
+        db.session.add(new_event)
+        db.session.commit()
+        return new_event.to_dict(), 201
 
-# class Yoga_SignUps(Resource):
-#     def get(self):
-#         pass
+# communityeventsbyID tested and working
+class CommunityEventsById(Resource):
+    def get(self, id):
+        event_by_id = Community_Event.query.filter_by(id=id).first()
+        if event_by_id == None:
+            return {'error':['event not found']}, 404
+        else:
+            return event_by_id, 200
 
-#     def post(self):
-#         pass
+    def patch(self, id):
+        event_by_id = Community_Event.query.filter_by(id=id).first()
+        if event_by_id == None:
+            return {'error':['event not found']}, 404
+        else:
+            fields = request.get_json()
+            try:
+                for attr in fields:
+                    setattr(event_by_id, attr, fields[attr])
+                    db.session.add(event_by_id)
+                    db.session.commit()
+                return event_by_id.to_dict(), 200
+            except ValueError:
+                return {'error':['validation error']}, 422
 
-# class YogaSignUpsById(Resource):
-#     def get(self):
-#         pass
+    def delete(self, id):
+        event_by_id = Community_Event.query.filter_by(id=id).first()
+        if event_by_id == None:
+            return {'error':['event not found']}, 404
+        else:
+            db.session.delete(event_by_id)
+            db.session.commit()
+            return {}, 200
 
-#     def patch(self):
-#         pass
+#yoga signups tested and working
+class Yoga_SignUps(Resource):
+    def get(self):
+        yoga_signups = [y.to_dict() for y in Yoga_SignUp.query.all()]
+        return yoga_signups, 200
 
-# class CommunityEventSignUps(Resource):
-#     def get(self):
-#         pass
+    def post(self):
+        fields = request.get_json()
+        # print(fields)
+        # user = db.session.get(User, fields['user_id'])
+        user = User.query.filter_by(id = fields['user_id']).first()
+        yoga_class = Yoga_Class.query.filter_by(id = fields['yoga_class_id']).first()
+        if user and yoga_class:
+            new_signup = Yoga_SignUp(
+                user_id=fields['user_id'],
+                yoga_class_id=fields['yoga_class_id']
+            )
+            db.session.add(new_signup)
+            db.session.commit()
+            return new_signup.to_dict(), 200
+        else:
+            return {'error': ['validation error or class/user does not exist']}, 404
 
-#     def post(self):
-#         pass
 
-# class CommunityEventSignUpsById(Resource):
-#     def get(self):
-#         pass
+#yogasignupsbyid routes tested and working
+class YogaSignUpsById(Resource):
+    #not adding delete bc I want to be able to track who canceled
+    def get(self, id):
+        signups_by_id = Yoga_SignUp.query.filter_by(id=id).first()
+        if signups_by_id == None:      
+            return {'error':['yoga class signup not found']}, 404
+        else:
+            return signups_by_id.to_dict(), 200
 
-#     def patch(self):
-#         pass
+    def patch(self, id):
+        signups_by_id = Yoga_SignUp.query.filter_by(id=id).first()
+        if signups_by_id == None:
+            return {'error':['signup not found']}, 404
+        else:
+            fields = request.get_json()
+            try:
+                for attr in fields:
+                    setattr(signups_by_id, attr, fields[attr])
+                    db.session.add(signups_by_id)
+                    db.session.commit()
+                return signups_by_id.to_dict(), 200
+            except ValueError:
+                return {'error': ['validation error']}, 404
+
+# communityeventsignups routes tested and working
+class CommunityEventSignUps(Resource):
+    def get(self):
+        event_signups = [e.to_dict() for e in Community_Event_SignUp.query.all()]
+        return event_signups, 200
+
+    def post(self):
+        fields = request.get_json()
+        user = User.query.filter_by(id=fields['user_id']).first()
+        event = Community_Event.query.filter_by(id=fields['community_event_id']).first()
+        if user and event:
+            new_event = Community_Event_SignUp(
+                user_id=fields['user_id'],
+                community_event_id=fields['community_event_id'],
+                user_status=fields['user_status']
+            )
+            db.session.add(new_event)
+            db.session.commit()
+            return new_event.to_dict(), 200
+        else:
+            return {'error': ['user/event does not exist']}, 404
+
+
+#communityeventsignupsbyID tested and working
+class CommunityEventSignUpsById(Resource):
+    #not adding delete bc I want to be able to track who canceled
+    def get(self, id):
+        signups_by_id = Community_Event_SignUp.query.filter_by(id=id).first()
+        if signups_by_id == None:
+            return {'error':['event signup not found']}, 404
+        else:
+            return signups_by_id.to_dict(), 200
+
+    def patch(self, id):
+        signups_by_id = Community_Event_SignUp.query.filter_by(id=id).first()
+        if signups_by_id == None:
+            return {'error':['signup not found']}, 404
+        else:
+            fields = request.get_json()
+            try:
+                for attr in fields:
+                    setattr(signups_by_id, attr, fields[attr])
+                    db.session.add(signups_by_id)
+                    db.session.commit()
+                return signups_by_id.to_dict(), 200
+            except ValueError:
+                return {'error': ['validation error']}, 404
+            
 
 
 
@@ -132,6 +339,22 @@ def not_found(e):
 # @app.route('/<path:path>')
 # def catch_all(path):
 #     return render_template("index.html")
+
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/checksession')
+api.add_resource(Logout, '/logout')
+api.add_resource(Users, '/users')
+api.add_resource(UsersById, '/users/<int:id>')
+api.add_resource(Yoga_Classes, '/yogaclasses')
+api.add_resource(YogaClassesById, '/yogaclasses/<int:id>')
+api.add_resource(Community_Events, '/communityevents')
+api.add_resource(CommunityEventsById, '/communityevents/<int:id>')
+api.add_resource(Yoga_SignUps, '/yogasignups')
+api.add_resource(YogaSignUpsById, '/yogasignups/<int:id>')
+api.add_resource(CommunityEventSignUps, '/communityeventsignups')
+api.add_resource(CommunityEventSignUpsById, '/communityeventsignups/<int:id>')
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
