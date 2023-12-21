050e7f18253828b640a053e4477822c9d50b8669
Brandi L Mason <masonbrandi66@gmail.com>
2023-12-21 12:20:45 -0600

    cleaned up files by removing comments

--- client/src/components/pages/calendar/Cell.jsx
+++ client/src/components/pages/calendar/Cell.jsx
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import clsx from 'clsx';
- 
-//  const Cell=({ className, children })=> {
-//    return (
-//      <div className={clsx("h-30 flex items-center justify-center", className)}>
-//         {children}
-//      </div>
-//    )
-//  }
- 
-//  export default Cell
 import React from 'react'
 
 function Cell({prop}) {
@@ -17,12 +5,7 @@ function Cell({prop}) {
     <div>Cell
       <div className='cards_item'>
           <aside className="cards_item_pic-wrap" data-category={prop.class}>
-            {/* <img src={props.src} alt='Adventure Image' className="cards_item_img" /> */}
           </aside>
-          {/* <div className='cards_item_info'>
-            <h5 className='cards_item_text'>{classes.day}</h5>
-            <p className='cards_item_description'>{classes.}</p>
-          </div> */}
       </div>
 
     </div>
