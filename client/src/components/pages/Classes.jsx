741317e24c624b3b12ec7b588057011979fcd7f5
Brandi L Mason <masonbrandi66@gmail.com>
2023-12-23 12:27:37 -0600

    added new class description

--- client/src/components/pages/Classes.jsx
+++ client/src/components/pages/Classes.jsx
@@ -16,6 +16,9 @@ function Classes() {
             Empower yourself with this musically driven, soul nourishing, upbeat class. Flow with breath and dig deep for 60 fun and challenging minutes.
 
             All levels welcome. Class heated to 90 degrees +.
+            <br></br>
+            Slow Power Flow
+            60 minute class designed for all levels. This class is structured similar to our power flow class, but taught at a much more intentional pace. We will spend more time in poses, as well as offer more modifications over the course of this class. Room is warmed to 80 degrees +.
 
 
             
